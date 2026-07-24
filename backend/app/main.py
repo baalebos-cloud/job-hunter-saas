6f0bcb59fee5369feb6da69f04398017e340695b
Oluwadare T. Jayeola <69169679+baalebos-cloud@users.noreply.github.com>
2026-07-24 07:49:48 +0100

    Change CORS port from 8000 to 8080

--- backend/app/main.py
+++ backend/app/main.py
@@ -135,7 +135,7 @@ else:
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
-        "http://localhost:8000",
+        "http://localhost:8080",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
     ]
